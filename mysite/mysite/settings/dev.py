78fab30d7667bf460a57389b366552a5fd56a236
Mshanga Makonde <41159327+mshanga@users.noreply.github.com>
2023-01-18 12:43:37 +0300

    first commit

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-v(s4ncfo&hp)bm&3##*_q+5=z4^)gvr-%@h!x#=nebrb2k=@x5"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
